b94ff4ae01d32dcf2e7e9e88003f868264124a12
Anatomee-design <davidsamuelchollom@gmail.com>
2025-07-08 13:14:07 +0100

    edit

---
+++ loops-in-javascript/script.js
@@ -0,0 +1,14 @@
+for (var i = 0; i <10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+var count = 10;
+    while (count >= 1){
+        console.log(count);
+        count--;
+    };
+var num =5;
+do{
+  console.log(num);
+} while (num>2)
